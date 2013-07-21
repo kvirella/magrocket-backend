3933eafe5e257d1e4af911992a7c17b10d7d0f66
Andrew Krowczyk <andrew@nin9creative.com>
2013-07-21 10:44:00 -0500

    Bug Fix
    Fix for issue of clicking logo which took you to the Admin dashboard instead of the user dashboard (/home).  Mentioned by tekdope.

--- source/application/views/admin/menu/home_menu.php
+++ source/application/views/admin/menu/home_menu.php
@@ -3,7 +3,7 @@
         <div class="container"><a class="btn btn-navbar"
                                   data-toggle="collapse" data-target=".nav-collapse"> <span
                     class="icon-bar"></span> <span class="icon-bar"></span> <span
-                    class="icon-bar"></span> </a> <a class="brand" href="<?php echo base_url(); ?>index.php/admin/dashboard"></a>
+                    class="icon-bar"></span> </a> <a class="brand" href="<?php echo base_url(); ?>index.php/admin/home"></a>
                     <!--<a class="brand" href="<?php echo base_url(); ?>index.php/admin/home"><?php echo $this->lang->line('__LBL_PROJECT_NAME__'); ?></a>-->
             <div class="nav-collapse collapse">
                 <ul class="nav">
